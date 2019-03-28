d6d109eefece5bd7b0428023d20c6bd90e34605b
Joy Lin <vvlnote@hotmail.com>
2019-03-28 00:10:01 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,7 +1,14 @@
 class Vehicle
   
+  attr_accessor :wheel_size, :wheel_number
+  
   def initialize(wheel_size, number)
-    
+    @wheel_size = wheel_size
+    @wheel_number = number
+  end
+  
+  def go
+    ""
   end
 
 end
